a97d97155e8d846f204807736743cda9902a6a75
Malcolm <malcolmmorton77@gmail.com>
2024-05-07 00:19:37 -0500

    Add data to the csv and add a name variable

--- src/rocket.cpp
+++ src/rocket.cpp
@@ -1,12 +1,38 @@
 #include <iostream>
 #include <sstream> //std::stringstream; std::stringbuf
-#include <string> // std::string
+#include <string> //std::string
+#include <fstream> //ifstream
 #include "menu.hpp"
 
-int main(){
-    // Here we need to read in a csv
+int main(int num_args, char *arg_string[]){
+    //Grab csv name from arguments
+    if(num_args != 2)
+    {
+        std::cout << "Error, not the correct number of arguments.\n";
+        exit(1);
+    }
+    
+    std::cout << "Processing csv " << arg_string[1] << "\n"
+              << "If this is not correct, the program will terminate undesirably.\n";
+    std::string csv_name = arg_string[1];
+    std::ifstream csv_reader;
+    std::string line;
+    
+    csv_reader.open(csv_name, std::ifstream::in); //open the file
+    std::getline(csv_reader, line); //throw the header out?
+    if(csv_reader.is_open()){
+        while(std::getline(csv_reader, line)){
+            Menu a{};
+            a.assignValues(line);
+            std::cout << "Assigned values for " << line << std::endl;
+        }
+    }
+
+
+    csv_reader.close();
+
     Menu a{}; // empty parentheses were disambiguated as a function declaration, use {}
-    Menu m{773, 4, 2, 0, 7};
+    Menu m{"Eaurius", 773, 4, 2, 0, 7};
     a.print();
     m.print();
     return 0;
