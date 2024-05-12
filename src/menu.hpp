a6187206ff36499e9a03e362cd68f32d9481ee02
Malcolm <47834908+malcolmmorton77@users.noreply.github.com>
2024-05-12 18:49:47 -0500

    Hat Trick Points (#2)
    * Update README with link to Rocket League point structure
    
    * Implement calculation for hat trick points

--- src/menu.hpp
+++ src/menu.hpp
@@ -12,12 +12,26 @@ private:
     int shots{};
 
 public:
+
+    /* name: Menu
+    * description: default constructor*/
     Menu(std::string="", int score=0, int goals=0, int assists=0, int saves=0, int shots=0);
+
+    /* name: ~Menu
+    * description: default destructor*/
     ~Menu();
 
+    /* name: assignValues
+    * description: assigns values using Boost libs to parse line
+    * into a Menu object*/
     void assignValues(std::string);
+
+    /* name: print
+     * description: prints the values associated with the Player*/
     void print() const;
 
+    /* list of getters
+     * used to get particular data from the Menu objects*/
     std::string getName() const { return name; }
     int getScore() const { return score; }
     int getGoals() const { return goals; }
@@ -25,6 +39,11 @@ public:
     int getSaves() const { return saves; }
     int getShots() const { return shots; }
     
+    /* name: hatTrickPoints
+    * description: returns the amount of points earned from hat tricks
+    * not including the actual goals themselves*/
+    int hatTrickPoints() const;
+
 };
 
 #endif
